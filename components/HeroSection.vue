6293697ad6532760b271ab41955067d9f1981906
zozo404 <enzocuny66@gmail.com>
2022-11-22 18:00:32 +0100

    ✨ | add sotre vuex and components

---
+++ components/HeroSection.vue
@@ -0,0 +1,68 @@
+<template>
+  <div
+    class="
+      hero
+      relative
+      max-h-screen
+      flex flex-col
+      justify-center
+      items-center
+      text-center
+      mx-auto
+      bg-cover
+    "
+  >
+    <div class="relative m-10 md:m-20">
+      <div class="relative">
+        <img class="absolute left-0 top-0" src="Repeat_Grid_2.png" alt="" />
+      </div>
+      <div class="z-10 relative">
+        <h1 class="text-5xl text-white m-3 font-bold md:text-6xl">
+          Unique Essence Store
+        </h1>
+        <p class="text-white subtitle">...your one stop shop for all</p>
+      </div>
+      <div class="circle absolute z-0 right-0 top-0"></div>
+      <div class="links mt-10">
+        <NuxtLink to="/" class="button--hero bg-button relative z-10">
+          View Collections
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+    
+<script>
+export default {
+  name: "HeroSection",
+};
+</script>
+    
+<style scoped>
+/* Sample `apply` at-rules with Tailwind CSS
+    .container {
+    @apply min-h-screen flex justify-center items-center text-center mx-auto;
+    }
+    */
+.hero {
+  background-color: #9c7474dc;
+}
+.circle {
+  width: 10em;
+  height: 10em;
+  border-radius: 5em;
+  background: #b8738d;
+}
+.title {
+  font-family: "Nunito";
+  display: block;
+  font-weight: 700;
+  font-size: 50px;
+  letter-spacing: 1px;
+  line-height: 1em;
+}
+.subtitle {
+  font-weight: 100;
+  word-spacing: 2px;
+}
+</style>
---
+++ store/index.js
@@ -0,0 +1,46 @@
+export const state = () => ({
+    allProducts: [],
+    featuredProducts: [],
+    menProducts: [],
+    womenProducts: [],
+    cartItems: [],
+})
+
+export const getters = {
+    /* 
+      return items from store
+    */
+    allProducts: (state) => state.allProducts,
+    featuredProducts: (state) => state.featuredProducts,
+    menProducts: (state) => state.menProducts,
+    womenProducts: (state) => state.womenProducts,
+    getCart: (state) => state.cartItems,
+    getCartTotal: (state) =>
+        state.cartItems.length < 1
+            ? '0'
+            : state.cartItems
+                .map((el) => el.price * el.quantity)
+                .reduce((a, b) => a + b),
+}
+
+export const actions = {
+    async addItemToCart({ commit }, cartItem) {
+        await commit('setCartItem', cartItem)
+    },
+    async deleteCartItem({ commit }, id) {
+        await commit('removeCartItem', id)
+    },
+}
+
+export const mutations = {
+    setProducts: (state, products) => (state.allProducts = products),
+    setFeaturedProducts: (state, products) => (state.featuredProducts = products),
+    setMenProducts: (state, products) => (state.menProducts = products),
+    setWomenProducts: (state, products) => (state.womenProducts = products),
+    setCartItem: (state, item) => state.cartItems.push(item),
+    removeCartItem: (state, id) =>
+        state.cartItems.splice(
+            state.cartItems.findIndex((el) => el.id === id),
+            1
+        ),
+}
